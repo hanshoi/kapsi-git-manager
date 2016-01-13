4b99b337709305390bf92219f9200e06bb57e329
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-01-13 19:28:19 +0200

    added a basic structure using http authentication

--- kapsi_git_manager/kgm.py
+++ kapsi_git_manager/kgm.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template, url_for, flash
+
+from authentication import requires_auth
+
 app = Flask(__name__)
 
 
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DEBUG=True,
+    SECRET_KEY='development key',
+))
+app.config.from_envvar('KGM_SETTINGS', silent=True)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('layout.html')
+
+
+@app.route('/home')
+@requires_auth
+def home():
+    flash("you are authed")
+    return render_template('layout.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
