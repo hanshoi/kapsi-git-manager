0a15f04454241da0a68ae402bdec59202b1866e2
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-01-14 11:30:46 +0200

    changed to use Flask-HTTPAuth package for http basic authentication and changed local exection out of the package

--- kapsi_git_manager/kgm.py
+++ kapsi_git_manager/kgm.py
@@ -1,29 +1,17 @@
-from flask import Flask, request, redirect, render_template, url_for, flash
+from __future__ import absolute_import
 
-from authentication import requires_auth
+from flask import Flask, request, redirect, render_template, url_for, flash
+from .authentication import auth
 
 app = Flask(__name__)
 
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DEBUG=True,
-    SECRET_KEY='development key',
-))
-app.config.from_envvar('KGM_SETTINGS', silent=True)
-
-
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return render_template('layout.html', authed=False)
 
 
 @app.route('/home')
-@requires_auth
+@auth.login_required
 def home():
-    flash("you are authed")
-    return render_template('layout.html')
-
-
-if __name__ == '__main__':
-    app.run()
+    return render_template('layout.html', authed=True)
