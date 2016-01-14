fd2eec00a7593d7a8c8ed25ee057cba73fb2bbdd
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-01-14 18:54:44 +0200

    added missing templates folder into setup.py also the dependencies

--- setup.py
+++ setup.py
@@ -14,18 +14,26 @@ def read(fname):
 
 setup(
     name="kapsi_git_manager",
-    version="0.0.1",
+    version="0.0.2",
     author="Antti Ruotsalainen",
     author_email="antti.ruotsalainen@hanshoi.net",
     description=("Simple git management application to be used in Kapsi hosting."),
     license="MIT",
     keywords="git management kapsi",
     url="http://packages.python.org/kapsi_git_manager",
-    packages=['kapsi_git_manager'],
+    packages=['kapsi_git_manager', 'tests'],
+    package_data={'kapsi_git_manager': ['license.txt', 'templates/*.html']},
+    include_package_data=True,
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'Flask',
+        'flup<=1.0.2',
+        'Flask-HTTPAuth',
+        'GitPython'
     ],
 )
