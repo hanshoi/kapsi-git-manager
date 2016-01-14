8dc99d39683943d622f65686d7846bdfc12e5b3b
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-01-14 12:10:36 +0200

    added a sample configuration

--- kapsi_git_manager/kgm.py
+++ kapsi_git_manager/kgm.py
@@ -1,9 +1,17 @@
 from __future__ import absolute_import
+import os
 
 from flask import Flask, request, redirect, render_template, url_for, flash
 from .authentication import auth
 
 app = Flask(__name__)
+
+# configuration
+SECRET_KEY = 'development key'
+GIT_FOLDER = os.path.join(os.path.abspath(__file__), 'git')
+
+app.config.from_object(__name__)
+app.config.from_envvar('KGM_SETTINGS', silent=True)
 
 
 @app.route('/')
